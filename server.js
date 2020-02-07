38cead99c810ed71c28607bf02a050a9ce71135e
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-02-06 19:18:29 -0600

    add http stuff

--- server.js
+++ server.js
@@ -17,3 +17,14 @@ app.listen(PORT, function() {
   // console.log listen port
   console.log("Server listening on: http://localhost:" + PORT);
 });
+
+/* Load the HTTP library */
+var http = require("http");
+
+/* Create an HTTP server to handle responses */
+
+http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello World");
+  response.end();
+}).listen(8888);
