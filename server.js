84392d9f3923bc6f78ec5cc982e762cf30f62f54
Kevin Suh <kevinsuh2018@u.northwestern.edu>
2020-02-08 12:31:33 -0600

    First working backend

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // requirements
 const express = require("express");
+const db = require("./models")
 
 // open port
 const PORT = process.env.PORT || 8080;
@@ -10,10 +11,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Declare static files
+app.use(express.static("public"));
 
+// Routes
+require("./routes/api-routes")(app)
+require("./routes/html-routes")(app)
 
-// Start server
-app.listen(PORT, function() {
-  // console.log listen port
-  console.log("Server listening on: http://localhost:" + PORT);
+// Sync sequelize files and then start server
+db.sequelize.sync({}).then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
