f53c623ae4eeb83ab93065ed4eb5f2535f0a14a2
unknown <yhlee89.dho@gmail.com>
2020-02-10 21:51:00 -0600

    added playlist portion of js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -6,95 +6,160 @@ $(document).ready(function() {
   // references for the search input
   const userSearch = $("#searchInput");
   const newSong = $("newSong");
+  const newYoutube = $("newYoutube");
   const songContainer = $(".song-container");
   // adding listners for submit of search, save result and delete saved
-  $(document).on("submit", "search-bar", userInputSearch);
+  $(document).on("submit", "#search-bar", userInputSearch);
   $(document).on("click", ".add-song", addSongClick);
-  $(document).on("click", ".delete-song", deleteSongClick);
 
-  // guessing this is how to do the search with npm youtube-search-api-with-axios
+  // guessing this is how we do the search with npm youtube-search-api-with-axios
   searchYoutube();
 
 
-// function to use submitted for search. might not be required because of npm module
-function userInputSearch(event) {
-  event.preventDefault();
-  // dont do shit if nothing is searched
-  if (!searchInput.val().trim().trim()) {
-    return;
-  }
-  useSearch({
-    title: searchInput.val().trim()
-  });
-}
-
-function useSearch(searchData) {
-  $.post("/api/search", searchData)
-    .then(getSearch)
-}
-
-// create a tr row to display searched history
-function createSearchRow(searchData) {
-  const newTr = $("<tr>");
-  newTr.data("search", searchData);
-  newTr.append("<td> " + searchData.newSong + "</td>");
-
-  // btn to add and delete searched song
-  newTr.append("<td><a style='cursor:pointer;color:green' class='add-song'>Add Song</a></td>");
-  newTr.append("<td><a style='cursor:pointer;color:red' class='delete-song'>Delete Song</a></td>");
-  return newTr;
-}
-
-// function to display search result
-function getSearch() {
-  $.get("/api/songs", function(data) {
-    var rowsToAdd = [];
-    for (var i = 0; i < data.length; i++) {
-      rowsToAdd.push(createSearchRow(data[i]));
+  // function to use submitted for search. might not be required because of npm module
+  function userInputSearch(event) {
+    event.preventDefault();
+    // dont do shit if nothing is searched
+    if (!userSearch.val().trim().trim()) {
+      return;
     }
-    renderSearchList(rowsToAdd);
-    nameInput.val("");
-  });
-}
-
-// render list of searchs to the page
-function renderSearchList(rows) {
-  searchList.children().not(":last").remove();
-  searchContainer.children(".alert").remove();
-  if (rows.length) {
-    console.log(rows);
-    searchList.prepend(rows);
+    useSearch({
+      title: userSearch.val().trim()
+    });
   }
-  else {
-    return;
-    // change to 
-    // renderEmpty(); if necessary to specify when there is nothing searched. dont think this is possible for youtube
+
+  function useSearch(searchData) {
+    $.post("/api/search", searchData)
+      .then(getSearch)
   }
-}
 
-// assuming all youtube searchs always return something
-// function renderEmpty() {blahblah}; check 14-01-14-solved for example
+  // create a card for the searched history
+  function createSearchCard(searchData) {
+    newCard.data("search", searchData);
 
-// Function for handling what happens when the add button is pressed
-function addSongClick() {
-  var listItemData = $(this).parent("td").parent("tr").data("song");
-  var id = listItemData.id;
-  $.ajax({
-    method: "POST",
-    url: "/api/songs/" + id
-  })
-    .then(getSongs);
-};
+    // elements for card
+    const newCard = $("<div>").attr("class", "card searchCard col-mt-6");
+    const cardBodyEl = $("<div>").attr("class", "card-body mb-4");
+    let cardNameEl = $("<div>").attr("class", "card-title").text(searchData.newSong);
 
-// Function for handling what happens when the delete button is pressed
-function deleteSongClick() {
-  var listItemData = $(this).parent("td").parent("tr").data("song");
-  var id = listItemData.id;
-  $.ajax({
-    method: "DELETE",
-    url: "/api/songs/" + id
-  })
-    .then(getSongs);
-};
+    // the link to youtube (probably should change this to a actialy html video thing)
+    let cardLinkEl = $("<p>").attr("class", "youtube-link").text(searchData.newYoutube);
+
+    // add button
+    let cardAddButton = $("button").attr("class", "btn btn-light add-song").attr("style", "cursor:pointer;color:green").text("Add Song");
+
+    // append elements
+    newCard.append(cardBodyEl);
+    cardBodyEl.append(cardNameEl, cardLinkEl, cardAddButton);
+
+    return newCard;
+  }
+
+  // function to display search result
+  function getSearch() {
+    $.get("/api/songs", function(data) {
+      var cardsToAdd = [];
+      for (var i = 0; i < data.length; i++) {
+        cardsToAdd.push(createSearchCard(data[i]));
+      }
+      renderSearchList(cardsToAdd);
+      userSearch.val("");
+    });
+  }
+
+  // render list of searchs to the page in our case its just 1
+  function renderSearchList(rows) {
+    searchList.children().not(":last").remove();
+    searchContainer.children(".alert").remove();
+  }
+
+  // Function for handling what happens when the add button is pressed
+  function addSongClick() {
+    const listItemData = $(this).parent("td").parent("tr").data("song");
+    const id = listItemData.id;
+    $.ajax({
+      method: "POST",
+      url: "/api/songs/" + id
+    })
+      .then(getSongs);
+  };
+
+
+
+// -------------------------------------------------------------------------------------
+// playlist portion below
+// -------------------------------------------------------------------------------------
+
+  // references for playlist
+  const playlistSong = $("#playlist-song");
+  const songList = $("songBody");
+  const playlistContainer = $(".playlist-container");
+  // adding listners for play song and delete from playlist
+  $(document).on("click", ".play-song", playSongClick);
+  $(document).on("click", ".delete-song", deleteSongClick);
+  // addling listners for play all songs on list
+  $(document).on("click", ".play-all-songs", playAllSongsClick);
+
+  // getting playlist
+  getPlaylist();
+
+  // create a tr row to display playlist
+  function createPlaylistRow(playlistData) {
+    const newTr = $("<tr>");
+    newTr.data("search", playlistData);
+    newTr.append("<td> " + playlistData.name + "</td>");
+    newTr.append("<td> " + playlistData.artist + "</td>");
+    // btn to play song on playlist and delete song
+    newTr.append("<td><a style='cursor:pointer;color:green' class='play-song'>Play Song</a></td>");
+    newTr.append("<td><a style='cursor:pointer;color:red' class='delete-song'>Delete Song</a></td>");
+    return newTr;
+  }
+
+
+  // function to display playlist
+  function getPlaylist() {
+    $.get("/api/songs", function(data) {
+      const rowsToAdd = [];
+      for (var i = 0; i < data.length; i++) {
+        rowsToAdd.push(createPlaylistRow(data[i]));
+      }
+      renderPlaylist(rowsToAdd);
+      playlistSong.val("");
+    });
+  }
+
+  // render list of searchs to the page
+  function renderPlaylist(rows) {
+    songList.children().not(":last").remove();
+    playlistContainer.children(".alert").remove();
+    if (rows.length) {
+      console.log(rows);
+      searchList.prepend(rows);
+    }
+    else {
+      renderEmpty();
+    }
+  }
+
+  // function for when there is nothing in playlist
+  function renderEmpty() {
+    const alertDiv = $("<div>");
+    alertDiv.addClass("alert alert-danger").text("The Playlist is Empty!");
+    playlistContainer.append(alertDiv);
+  }
+
+  // Function for handling what happens when the delete button is pressed
+  function deleteSongClick() {
+    const listItemData = $(this).parent("td").parent("tr").data("song");
+    const id = listItemData.id;
+    $.ajax({
+      method: "DELETE",
+      url: "/api/songs/" + id
+    })
+      .then(getPlaylist);
+  };
 
 });
+
+
+
