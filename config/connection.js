9208c301fa17007a5bf7ab2d31d45ef085edd096
Jacob Rosenberg <JMRosenberg@alumni.fullsail.edu>
2020-02-13 19:08:23 -0600

    add config folder

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+ {
+    "development": {
+      username: "root",
+      password: "Trainstogo1@",
+      database: "myplaylist",
+      host: "localhost",
+      port: 3306,
+      dialect: "mysql"
+    },
+    "test": {
+      username: "root",
+      password: "Trainstogo1@",
+      database: "database_test",
+      host: "127.0.0.1",
+      port: 3306,
+      dialect: "mysql"
+    },
+    "production": {
+      username: "root",
+      password: "Trainstogo1@",
+      database: "database_production",
+      host: "127.0.0.1",
+      port: 3306,
+      dialect: "mysql"
+    }
+}
